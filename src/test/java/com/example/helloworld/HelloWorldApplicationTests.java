33fbcefec4d358ed85aae7e14357f1625cb3814a
Damish <damishs88@gmail.com>
2022-02-28 08:41:30 +0530

    added hello world app

---
+++ src/test/java/com/example/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void test1() {
+		System.out.println("Hello world test 1");
+	}
+
+}
